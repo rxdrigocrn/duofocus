27ed65bda732958f803b1680cbaf6c019361d6c4
Erfon Spanos <erfonspanos@gmail.com>
2025-05-15 00:14:02 -0300

    Att: Botões da tela de inicio fixados + margin, padding de alguns elementos e cores dos botoes trocados

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const header = () => {
   return (
-    <header className="flex justify-between border-b-1">
+    <header className="flex justify-between border-b-2">
       <div className="flex items-center mx-[4rem] my-[1rem]" >
         <img src="/logo.svg" alt="Logo" className="h-[47px]" />
         <h2>DuoFocus</h2>
