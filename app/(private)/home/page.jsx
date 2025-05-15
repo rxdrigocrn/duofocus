986ab5b0b3d7c03520299d985d8917daef9ce942
Erfon Spanos <erfonspanos@gmail.com>
2025-05-15 15:48:37 -0300

    Add: 2 textos na home

--- app/(private)/home/page.jsx
+++ app/(private)/home/page.jsx
@@ -8,13 +8,23 @@ const home = () => {
       <Header />
 
       <div className="flex min-h-[calc(100vh-82px)]">
-        <section className="w-1/2 flex justify-center items-center" style={{borderRight: '3px solid #1B284E'}}>
-            
-        </section>
+        <section
+          className="w-1/2 flex justify-center items-center"
+          style={{ borderRight: "3px solid #1B284E" }}
+        ></section>
 
-        <section className="w-1/2 flex justify-center items-center">
+        <section className="w-1/2 flex justify-center items-center relative">
+          
+          <div className="absolute top-10">
+            <h3 className="text-white text-xl">Today's progress:</h3>
+          </div>
+
           <div className="flex flex-col gap-10">
-            <Button text="Body" icon="/bodyWhite.svg" bgColor="bg-[#FF6B6B]" />
+            <Button 
+              text="Body" 
+              icon="/bodyWhite.svg" 
+              bgColor="bg-[#FF6B6B]" 
+              />
 
             <Button
               text="Mind"
@@ -23,6 +33,11 @@ const home = () => {
               iconSize="w-6 h-6"
             />
           </div>
+
+          <div className="absolute bottom-20 max-w-[250px]">
+            <h3 className="text-[#9CA3AF] text-xl text-center">"Every small step brings you closer to your big dream."</h3>
+          </div>
+
         </section>
       </div>
     </div>
