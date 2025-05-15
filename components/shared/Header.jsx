47ba15526240374e644347ee9824752921fc0e6b
erfonspanos <erfonspanos@gmail.com>
2025-05-14 21:56:07 -0300

    Att: imagem ajustada

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const header = () => {
   return (
-    <header className="flex justify-between border-b-4">
-      <div className="flex items-center">
-        <img src="/logo.svg" alt="Logo" />
+    <header className="flex justify-between border-b-1">
+      <div className="flex items-center mx-[4rem] my-[1rem]" >
+        <img src="/logo.svg" alt="Logo" className="h-[47px]" />
         <h2>DuoFocus</h2>
       </div>
     </header>
