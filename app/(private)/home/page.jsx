2f8d5e09a06b1d41c14bc7c4e6fae8ae569895ee
Rodrigo <rodrigocirinocl@gmail.com>
2025-05-18 01:26:07 -0300

    add: buttons indo para as paginas corretamente

--- app/(private)/home/page.jsx
+++ app/(private)/home/page.jsx
@@ -1,8 +1,8 @@
 'use client'
 import React from "react";
-import Header from "@/components/shared/Header";
 import Button from "@/components/shared/Button";
 import TasksTable from "@/components/templates/TasksTable";
+import { redirect } from "next/navigation";
 import { useState } from "react";
 
 const HomePage = () => {
@@ -37,16 +37,16 @@ const HomePage = () => {
                         Good morning, Erfon! Be <br className="hidden sm:block" /> consistent. Success is built <br className="hidden sm:block" /> day by day.
                     </h1>
 
-                         <TasksTable
-                            title="Minhas Tarefas Diárias"
-                            tasks={filteredTasks}
-                            bgColor="bg-gray-50"
-                            textColor="text-gray-800"
-                            onFilter={handleFilterChange}
-                            activeFilter={activeFilter}
-                            onToggle={toggleTaskCompletion}
-                        />
- 
+                    <TasksTable
+                        title="Minhas Tarefas Diárias"
+                        tasks={filteredTasks}
+                        bgColor="bg-gray-50"
+                        textColor="text-gray-800"
+                        onFilter={handleFilterChange}
+                        activeFilter={activeFilter}
+                        onToggle={toggleTaskCompletion}
+                    />
+
                     <div className="mt-6 lg:absolute lg:bottom-10">
                         <h3 className="text-white text-lg md:text-xl text-center">
                             Big changes start with small actions.
@@ -63,20 +63,25 @@ const HomePage = () => {
                     </div>
 
                     <div className="flex flex-row lg:flex-col gap-4 lg:gap-10 w-full justify-center items-center">
-                        <Button
-                            text="Body"
-                            icon="/bodyWhite.svg"
-                            bgColor="bg-[#FF6B6B]"
-                            className="w-full sm:w-auto px-6"
-                        />
+                        <div onClick={() => redirect('/add-body-task')}>
+                            <Button
+                                text="Body"
+                                icon="/bodyWhite.svg"
+                                bgColor="bg-[#FF6B6B]"
+                                className="w-full sm:w-auto px-6"
 
-                        <Button
-                            text="Mind"
-                            icon="/mindWhite.svg"
-                            bgColor="bg-[#4D96FF]"
-                            iconSize="w-6 h-6"
-                            className="w-full sm:w-auto px-6"
-                        />
+                            />
+                        </div>
+
+                        <div onClick={() => redirect('/add-mind-task')}>
+                            <Button
+                                text="Mind"
+                                icon="/mindWhite.svg"
+                                bgColor="bg-[#4D96FF]"
+                                iconSize="w-6 h-6"
+                                className="w-full sm:w-auto px-6"
+                            />
+                        </div>
                     </div>
 
                     <div className="mt-8 lg:absolute lg:bottom-10 max-w-[250px]">
